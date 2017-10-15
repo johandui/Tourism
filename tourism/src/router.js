f7c0be794ebdbecc82aebe3d29af794a38cf92bd
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-15 23:47:28 +0800

    add router

---
+++ tourism/src/router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, Route} from 'react-router';
+
+import {Homepage} from './pages/Homepage';
+import {Secondpage} from './pages/Secondpage';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router></Router>
+        );
+    }
+}
+
+render(
+    <App/>, window.document.getElementById("root"));
