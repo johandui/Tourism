93019528174b94b10b7c780b028655e968e18242
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-15 15:12:00 +0800

    second page

--- tourism/src/pages/secondpage.js
+++ tourism/src/pages/secondpage.js
@@ -1,1 +1,13 @@
+import React, {Component} from 'react';
 
+class Secondpage extends Component {
+    render() {
+        return (
+            <div className="Secondpage">
+                <h1>its second page section</h1>
+            </div>
+        );
+    }
+}
+
+export default Secondpage;
