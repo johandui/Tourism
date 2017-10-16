d25b8acf4d52a3715a4aca180045a3fc6db57eef
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-16 19:05:34 +0800

    add cloud image

--- tourism/src/pages/Homepage.js
+++ tourism/src/pages/Homepage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export class Homepage extends React.Component {
   render() {
     return (
--- tourism/src/pages/Secondpage.js
+++ tourism/src/pages/Secondpage.js
@@ -5,11 +5,14 @@ export class Secondpage extends React.Component {
         return (
             <div className="Secondpage">
                 <div class="container">
-                    <div class="header">
-                        <span class="header-text">TOURISM</span>
-                        <img class="cloud" src="assets/Asset 2.svg"/>
+                    <div class="row wrapper">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 cloud_wrapper">
+                            <img class="cloud" src="assets/Asset 2.svg"/>
+                        </div>
+                        <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
+                            <span class="text">TOURISM</span>
+                        </div>
                     </div>
-
                 </div>
             </div>
         );
