8c002b8f436fc67ddee4b8c1557a7135d2a1bc0f
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-16 22:01:14 +0800

    add footer and step2

--- tourism/src/pages/Secondpage.js
+++ tourism/src/pages/Secondpage.js
@@ -11,9 +11,18 @@ export class Secondpage extends React.Component {
                         </div>
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                             <span class="header-txt">TOURISM</span><br/><br/>
-                            <span class="step1">Step 1</span><br/>
+                            <span class="steps">Step 1</span><br/>
                             <span class="days">How many days do you need to travel in Ulaanbaatar ?</span><br/><br/>
-                            <img class="cloud" src="assets/Asset 1.svg"/>
+                            <img class="cloud" src="assets/Asset 1.svg"/><br/><br/>
+                            <span class="steps">Step 2</span><br/>
+                            <span class="money">How much money will you spend to this trip ?</span><br/><br/>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid footer-img">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <img class="cloud" src="assets/Asset 4.svg"/>
                         </div>
                     </div>
                 </div>
