e248f735aeebf2950efa030a749d403961622e91
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-16 23:14:14 +0800

    add asset7 (footer img)

--- tourism/src/pages/Secondpage.js
+++ tourism/src/pages/Secondpage.js
@@ -6,23 +6,24 @@ export class Secondpage extends React.Component {
             <div className="Secondpage">
                 <div class="container">
                     <div class="row header-wrapper">
-                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 cloud_wrapper">
+                        <div class="col cloud_wrapper">
                             <img class="cloud" src="assets/Asset 2.svg"/>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <div class="col-7">
                             <span class="header-txt">TOURISM</span><br/><br/>
                             <span class="steps">Step 1</span><br/>
                             <span class="days">How many days do you need to travel in Ulaanbaatar ?</span><br/><br/>
-                            <img class="cloud" src="assets/Asset 1.svg"/><br/><br/>
+                            <img  src="assets/Asset 1.svg"/><br/><br/>
                             <span class="steps">Step 2</span><br/>
                             <span class="money">How much money will you spend to this trip ?</span><br/><br/>
                         </div>
+                        <div class="col"></div>
                     </div>
                 </div>
                 <div class="container-fluid footer-img">
                     <div class="row">
-                        <div class="col-lg-12">
-                            <img class="cloud" src="assets/Asset 4.svg"/>
+                        <div class="col-12">
+                            <img src="assets/Asset 7.svg"/>
                         </div>
                     </div>
                 </div>
