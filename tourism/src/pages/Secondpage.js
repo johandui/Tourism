949695b080af2bbbb89a06ca9004205b4a25d3b5
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-16 19:44:58 +0800

    just commit

--- tourism/src/pages/Secondpage.js
+++ tourism/src/pages/Secondpage.js
@@ -5,17 +5,17 @@ export class Secondpage extends React.Component {
         return (
             <div className="Secondpage">
                 <div class="container">
-                    <div class="row wrapper">
+                    <div class="row header-wrapper">
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 cloud_wrapper">
                             <img class="cloud" src="assets/Asset 2.svg"/>
                         </div>
                         <div class="col-lg-6 col-md-3 col-sm-3 col-xs-3">
-                            <span class="text">TOURISM</span>
+                            <span class="header-txt">TOURISM</span>
                         </div>
                     </div>
                     <div class="row centered">
                         <div class="col-lg-2"></div>
-                        <div class="col-lg-8 d">
+                        <div class="col-lg-8">
                             <span class="step1">Step 1</span><br/>
                             <span class="days">How many days do you need to travel in Ulaanbaatar ?</span>
                         </div>
