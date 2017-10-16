03b1453264ac1fb772ca4c020ac8002b22b1c5ec
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-16 13:42:52 +0800

    add assets folder in public dir

--- tourism/src/pages/Secondpage.js
+++ tourism/src/pages/Secondpage.js
@@ -4,7 +4,13 @@ export class Secondpage extends React.Component {
     render() {
         return (
             <div className="Secondpage">
-                <h1>its second page section</h1>
+                <div class="container">
+                    <div class="header">
+                        <span class="header-text">TOURISM</span>
+                        <img class="cloud" src="assets/Asset 2.svg"/>
+                    </div>
+
+                </div>
             </div>
         );
     }
