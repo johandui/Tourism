b8cb0a038fc334cbef6f3ac566fccb3590ed6048
Ariunbold <ariuka.jagaa@gmail.com>
2017-10-20 23:06:31 +0800

    form added

--- tourism/src/pages/Lastpage.js
+++ tourism/src/pages/Lastpage.js
@@ -5,11 +5,24 @@ export class Lastpage extends React.Component{
         return(
             <div className="Lastpage">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-4"></div>
-                        <div class="col-8 map-wrapper">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15134.016654543831!2d106.92402010055793!3d47.89107614156682!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5fb3ba226fc5b98b!2sOrgil+SEO!5e0!3m2!1sen!2sus!4v1508505458544"
-                                    ></iframe>
+                    <div class="row map-row">
+                        <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <form class="contact-form">
+                                <label for="id_name">Name: *</label>
+                                <input type="text" name="name" id="id_name"/><br/>
+                                <label for="id_email">Email: *</label>
+                                <input type="text" name="email" id="id_email"/><br/>
+                                <label for="id_message">Message: *</label>
+                                <textarea cols="40" rows="5" name="message" id="id_message"></textarea>
+                                <button type="submit">Send</button>
+                            </form>
+                        </div>
+                        <div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="map-wrapper">
+                                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15134.016654543831!2d106.92402010055793!3d47.89107614156682!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5fb3ba226fc5b98b!2sOrgil+SEO!5e0!3m2!1sen!2sus!4v1508505458544"
+                                        allowFullScreen></iframe>
+                            </div>
+
                         </div>
                     </div>
                 </div>
