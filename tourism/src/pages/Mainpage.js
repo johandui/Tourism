1df1650642d891670fe7e5c99b8c465ae0b728d3
Ariunbold <ariuka.jagaa@gmail.com>
2018-01-21 22:35:54 +0800

    added attrs list

--- tourism/src/pages/Mainpage.js
+++ tourism/src/pages/Mainpage.js
@@ -2,7 +2,17 @@ import React from 'react';
 import axios from 'axios';
 
 export class Mainpage extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            attractions: [],
+            is_attr_delete_button_clicked: false
+        };
+    }
+
     componentDidMount() {
+        var global_this = this;
+        var result = -1;
         var answers = this.props.history.location.state.split("/");
         var startDate = new Date(0);
         startDate.setMilliseconds(answers[1]);
@@ -20,22 +30,66 @@ export class Mainpage extends React.Component {
             }
         })
         .then(function (response) {
-            console.log(response.data);
+            global_this.setState({
+                attractions: response.data
+            });
         })
         .catch(function (error) {
             console.log(error);
         });
     }
+
+    handleCheck(e) {
+        var pos = e.currentTarget.dataset.id-1;
+        var arr = this.state.attractions;
+        arr.splice(pos, 1);
+        this.setState({
+            attractions: arr
+        });
+    }
+
+    deleteButtonChecker(e) {
+        if(!this.state.is_attr_delete_button_clicked) {
+            e.currentTarget.classList.add("show_atts_remove_button");
+            this.setState({
+                is_attr_delete_button_clicked: true
+            });
+        }
+        else {
+            e.currentTarget.classList.remove("show_atts_remove_button");
+            this.setState({
+                is_attr_delete_button_clicked: false
+            });
+        }
+    }
+
     render() {
+        var count = 0;
+        var arr = this.state.attractions.map((att) => {
+            count++;
+            if(count==1 || count== this.state.attractions.length) {
+                return  <li class="atts_li" onClick={this.deleteButtonChecker.bind(this)}>
+                            <span class="atts_number unique_number">{count} </span>{att}
+                            <span class="atts_remove" onClick={this.handleCheck.bind(this)} data-id={count}>  x</span>
+                        </li>
+            }
+            else {
+                return  <li class="atts_li" onClick={this.deleteButtonChecker.bind(this)}>
+                            <span class="atts_number">{count} </span>{att}
+                            <span class="atts_remove" onClick={this.handleCheck.bind(this)} data-id={count}>  x</span>
+                        </li>
+            }
+        })
         return (
             <div className="Mainpage">
                 <div class="container">
-                    <div class="row header-wrapper">
-                        <div class="col">
+                    <div class="row">
+                        <div class="col-4" class="atts_div">
+                            <ul class="atts_ul">
+                                {arr}
+                            </ul>
                         </div>
-                        <div class="col-7">
-                        </div>
-                        <div class="col">
+                        <div class="col-8">
                         </div>
                     </div>
                 </div>
